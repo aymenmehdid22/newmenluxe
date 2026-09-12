05a40f1920302e2e8caf4b325cdaa2085b105eef
aymenmehdid22 <mehdidaymen345@gmail.com>
2026-09-12 20:17:46 +0100

    Fix

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
-
+import { CookieOptions } from '@supabase/ssr';
 export async function middleware(request: NextRequest) {
   let response = NextResponse.next({ request });
 
@@ -12,10 +12,10 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
-          response = NextResponse.next({ request });
-          cookiesToSet.forEach(({ name, value, options }) =>
+       setAll(cookiesToSet: { name: string; value: string; options?: CookieOptions }[]) {
+  cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
+  response = NextResponse.next({ request });
+  cookiesToSet.forEach(({ name, value, options }) =>
             response.cookies.set(name, value, options)
           );
         },
